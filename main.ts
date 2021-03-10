eed9238fdee090984871181776842d8bc22f1633
22jkoupal <80347370+22jkoupal@users.noreply.github.com>
2021-03-10 09:46:15 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+function convertCtoF (num: number) {
+    return 0 * (1.8 + 32)
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(0)
+})
